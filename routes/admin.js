c8770d9ccea3aa5744772744d6d8f8a33efb6caa
federicodonner <federico.donner@gmail.com>
2022-02-09 23:01:52 -0300

    build(admin, sale): update for psql

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,14 @@
 // Route file for operations to the user's collection
 var express = require("express");
 var router = express.Router();
-var db = require("../config/db");
+var client = require("../config/db");
 const { check, validationResult, isNumeric } = require("express-validator");
 var messages = require("../data/messages");
 
 router.post(
   "/payment",
   [check("collectionId").isNumeric(), check("ammount").isNumeric().isFloat()],
-  (req, res) => {
+  async (req, res) => {
     // Validates that the parameters are correct
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -21,49 +21,37 @@ router.post(
 
     // Verifies that the collection exists
     let sql = "SELECT * FROM collection WHERE id = " + collectionId;
-    let query = db.query(sql, (err, collections) => {
-      if (err) {
-        throw err;
-      }
-      // If there are no results, return error
-      if (!collections.length) {
-        return res.status(404).json({ message: messages.COLLECTION_PROBLEM });
-      }
+    let collections = await client.query(sql);
+    if (collections.err) {
+      throw collections.err;
+    }
+    // If there are no results, return error
+    if (!collections.rows.length) {
+      return res.status(404).json({ message: messages.COLLECTION_PROBLEM });
+    }
 
-      // Store the payment in the database
-      var today = new Date();
-      today = Math.round(today / 1000);
-      sql =
-        "INSERT INTO payment (date, ammount, collectionId) VALUES (" +
-        today +
-        "," +
-        ammount +
-        "," +
-        collectionId +
-        ")";
-      query = db.query(sql, (err) => {
-        if (err) {
-          throw err;
-        }
+    // Store the payment in the database
+    var today = new Date();
+    today = Math.round(today / 1000);
+    sql =
+      "INSERT INTO payment (date, ammount, collectionid) VALUES (" +
+      today +
+      "," +
+      ammount +
+      "," +
+      collectionId +
+      ") RETURNING date, ammount";
+    let paymentInsert = await client.query(sql);
+    if (paymentInsert.err) {
+      throw paymentInsert.err;
+    }
 
-        // After the gasto is inserted, return it to the UI
-        sql = "SELECT * FROM payment WHERE id = LAST_INSERT_ID()";
-        query = db.query(sql, (err, results) => {
-          if (err) {
-            throw err;
-          }
-          res.status(201).json(results[0]);
-        });
-      });
-    });
+    res.status(201).json(paymentInsert.rows[0]);
   }
 );
 
 // Post a sale
-router.post("/sale", (req, res) => {
-  // Gets the userId from the authentication middleware
-  var userId = req.userId;
-
+router.post("/sale", async (req, res) => {
   // Sold card ids for getting them from database
   var soldCardIds = "(";
 
@@ -90,161 +78,156 @@ router.post("/sale", (req, res) => {
   }
 
   sql =
-    "SELECT c.id, c.quantity, c.conditionId, c.languageId, c.foil, c.collectionId, c.scryfallId, g.name AS 'cardName', o.percent FROM card c LEFT JOIN cardGeneral g ON c.scryfallId = g.scryfallId LEFT JOIN collection o ON c.collectionId = o.id WHERE c.id in " +
+    "SELECT c.id, c.quantity, c.conditionid, c.languageid, c.foil, c.collectionid, c.scryfallid, g.name AS cardname, o.percent FROM card c LEFT JOIN cardgeneral g ON c.scryfallid = g.scryfallid LEFT JOIN collection o ON c.collectionid = o.id WHERE c.id in " +
     soldCardIds;
 
-  query = db.query(sql, (err, cardsInDb) => {
-    if (err) {
-      throw err;
-    }
+  let cardsInDb = await client.query(sql);
+  if (cardsInDb.err) {
+    throw cardsInDb.err;
+  }
 
-    // At this point:
-    // - user is superuser
-    // - soldCards has JSONs of the sold cards
-    // - cardsInDb has those same cards from the db
+  // At this point:
+  // - user is superuser (because of admin middleware)
+  // - soldCards has JSONs of the sold cards
+  // - cardsInDb has those same cards from the db
 
-    // Verifies that there are no repeat ids in the array
-    var repeatCards = false;
-    soldCards.forEach((cardCompared, indexCompared) => {
-      soldCards.forEach((cardToCompare, indexToCompare) => {
-        if (
-          cardCompared.id == cardToCompare.id &&
-          indexCompared != indexToCompare
-        ) {
-          repeatCards = true;
-        }
-      });
-    });
-
-    if (repeatCards) {
-      return res.status(400).json({ message: messages.SALE_REPEAT_CARDS });
-    }
-
-    // Verifies that the cards exist
-    var cardNotFound = null;
-    soldCards.forEach((soldCard) => {
-      var idFound = false;
-      cardsInDb.forEach((cardInDb) => {
-        if (soldCard.id == cardInDb.id) {
-          idFound = true;
-        }
-      });
-      if (!idFound) {
-        cardNotFound = soldCard;
+  // Verifies that there are no repeat ids in the array
+  var repeatCards = false;
+  soldCards.forEach((cardCompared, indexCompared) => {
+    soldCards.forEach((cardToCompare, indexToCompare) => {
+      if (
+        cardCompared.id == cardToCompare.id &&
+        indexCompared != indexToCompare
+      ) {
+        repeatCards = true;
       }
     });
-    if (cardNotFound) {
-      return res
-        .status(400)
-        .json({ message: messages.SEARCH_NOT_FOUND, card: cardNotFound });
-    }
-
-    // Verifies that there is enough quantity of each card in the collectino
-    var cardWithoutEnoughStock = null;
-    soldCards.forEach((soldCard) => {
-      cardsInDb.forEach((cardInDb) => {
-        // If it already found one, stop verifying
-        if (!cardWithoutEnoughStock) {
-          if (
-            soldCard.id == cardInDb.id &&
-            soldCard.saleQuantity > cardInDb.quantity
-          ) {
-            cardWithoutEnoughStock = cardInDb;
-          }
-        }
-      });
-    });
-    if (cardWithoutEnoughStock) {
-      return res.status(400).json({
-        message: messages.SALE_NOT_ENOUGH_STOCK,
-        card: cardWithoutEnoughStock,
-      });
-    }
-
-    // If al the data is correct, process the sale
-    var today = new Date();
-    today = Math.round(today / 1000);
-    soldCards.forEach((soldCard) => {
-      cardsInDb.forEach((cardInDb) => {
-        if (soldCard.id == cardInDb.id) {
-          sql =
-            "INSERT INTO sale (collectionId, scryfallId, price, percent, quantity, date, conditionId, languageId, foil) VALUES (" +
-            cardInDb.collectionId +
-            ",'" +
-            cardInDb.scryfallId +
-            "'," +
-            soldCard.price +
-            "," +
-            cardInDb.percent +
-            "," +
-            soldCard.saleQuantity +
-            "," +
-            today +
-            "," +
-            cardInDb.conditionId +
-            "," +
-            cardInDb.languageId +
-            "," +
-            cardInDb.foil +
-            ")";
-          query = db.query(sql, (err, cards) => {
-            if (err) {
-              throw err;
-            }
-          });
-          // Modify the stock of the card
-          if (soldCard.saleQuantity == cardInDb.quantity) {
-            sql = "DELETE FROM card WHERE id = " + cardInDb.id;
-          } else {
-            sql =
-              "UPDATE card SET quantity = " +
-              (cardInDb.quantity - parseInt(soldCard.saleQuantity)) +
-              " WHERE id = " +
-              cardInDb.id;
-          }
-          query = db.query(sql, (err, cards) => {
-            if (err) {
-              throw err;
-            }
-          });
-        }
-      });
-    });
-    return res.status(201).json({ message: messages.SALE_PROCESSED });
   });
+
+  if (repeatCards) {
+    return res.status(400).json({ message: messages.SALE_REPEAT_CARDS });
+  }
+
+  // Verifies that the cards exist
+  var cardNotFound = null;
+  soldCards.forEach((soldCard) => {
+    var idFound = false;
+    cardsInDb.rows.forEach((cardInDb) => {
+      if (soldCard.id == cardInDb.id) {
+        idFound = true;
+      }
+    });
+    if (!idFound) {
+      cardNotFound = soldCard;
+    }
+  });
+  if (cardNotFound) {
+    return res
+      .status(400)
+      .json({ message: messages.SEARCH_NOT_FOUND, card: cardNotFound });
+  }
+
+  // Verifies that there is enough quantity of each card in the collectino
+  var cardWithoutEnoughStock = null;
+  soldCards.forEach((soldCard) => {
+    cardsInDb.rows.forEach((cardInDb) => {
+      // If it already found one, stop verifying
+      if (!cardWithoutEnoughStock) {
+        if (
+          soldCard.id == cardInDb.id &&
+          soldCard.saleQuantity > cardInDb.quantity
+        ) {
+          cardWithoutEnoughStock = cardInDb;
+        }
+      }
+    });
+  });
+  if (cardWithoutEnoughStock) {
+    return res.status(400).json({
+      message: messages.SALE_NOT_ENOUGH_STOCK,
+      card: cardWithoutEnoughStock,
+    });
+  }
+
+  // If al the data is correct, process the sale
+  var today = new Date();
+  today = Math.round(today / 1000);
+  for (const soldCard of soldCards) {
+    for (const cardInDb of cardsInDb.rows) {
+      if (soldCard.id == cardInDb.id) {
+        sql =
+          "INSERT INTO sale (collectionid, scryfallid, price, percent, quantity, date, conditionid, languageid, foil) VALUES (" +
+          cardInDb.collectionid +
+          ",'" +
+          cardInDb.scryfallid +
+          "'," +
+          soldCard.price +
+          "," +
+          cardInDb.percent +
+          "," +
+          soldCard.saleQuantity +
+          "," +
+          today +
+          "," +
+          cardInDb.conditionid +
+          "," +
+          cardInDb.languageid +
+          "," +
+          cardInDb.foil +
+          ")";
+        let addCards = await client.query(sql);
+        if (addCards.err) {
+          throw addCards.err;
+        }
+        // Modify the stock of the card
+        if (soldCard.saleQuantity == cardInDb.quantity) {
+          sql = "DELETE FROM card WHERE id = " + cardInDb.id;
+        } else {
+          sql =
+            "UPDATE card SET quantity = " +
+            (cardInDb.quantity - parseInt(soldCard.saleQuantity)) +
+            " WHERE id = " +
+            cardInDb.id;
+        }
+        let deleteCards = await client.query(sql);
+        if (deleteCards.err) {
+          throw deleteCards.err;
+        }
+      }
+    }
+  }
+  return res.status(201).json({ message: messages.SALE_PROCESSED });
 });
 
 // Return user's details based on the token
 // Copied form userRoute here to use the superuser middleware
-router.get("/me", (req, res) => {
-  // Gets the userId from the authentication middleware
-  var userId = req.userId;
-  let sql = "SELECT * FROM user WHERE id = " + userId;
-  let query = db.query(sql, (err, users) => {
-    if (err) {
-      throw err;
-    }
-    // If there are no results, return error
-    if (!users.length) {
-      return res.status(401).json({ message: messages.UNAUTHORIZED });
-    }
-    // If there is a user, return it
-    delete users[0].passwordHash;
-    delete users[0].id;
-    res.status(200).json(users[0]);
-  });
+router.get("/me", async (req, res) => {
+  // Gets the playerId from the authentication middleware
+  var playerId = req.playerId;
+  let sql = "SELECT * FROM player WHERE id = " + playerId;
+  let users = await client.query(sql);
+  if (users.err) {
+    throw users.err;
+  }
+  // If there are no results, return error
+  if (!users.rows.length) {
+    return res.status(401).json({ message: messages.UNAUTHORIZED });
+  }
+  // If there is a user, return it
+  delete users.rows[0].passwordHash;
+  delete users.rows[0].id;
+  res.status(200).json(users.rows[0]);
 });
 
 // Return payments and sales from collections
-router.get("/pendingpayments", (req, res) => {
+router.get("/pendingpayments", async (req, res) => {
   let sql =
-    "SELECT u.name, one.collectionId, one.sales, one.commission, two.payments, (one.sales - one.commission - two.payments) AS 'outstanding' FROM (SELECT collectionId, SUM(price) AS 'sales', SUM(price*percent) AS 'commission' from sale GROUP BY collectionId) one LEFT JOIN (SELECT collectionId, SUM(ammount) AS 'payments' from payment GROUP BY collectionId) two ON one.collectionId = two.collectionId LEFT JOIN collection o ON one.collectionId = o.id LEFT JOIN user u ON o.userId = u.id";
-  let query = db.query(sql, (err, collections) => {
-    if (err) {
-      throw err;
-    }
-    return res.status(200).json(collections);
-  });
+    "SELECT u.name, one.collectionid, one.sales, one.commission, two.payments, (one.sales - one.commission - two.payments) AS outstanding FROM (SELECT collectionid, SUM(price) AS sales, SUM(price*percent) AS commission from sale GROUP BY collectionid) one LEFT JOIN (SELECT collectionid, SUM(ammount) AS payments from payment GROUP BY collectionid) two ON one.collectionid = two.collectionid LEFT JOIN collection o ON one.collectionid = o.id LEFT JOIN player p ON o.playerid = p.id";
+  let collections = await client.query(sql);
+  if (collections.err) {
+    throw collections.err;
+  }
+  return res.status(200).json(collections.rows);
 });
 
 module.exports = router;
--- routes/sale.js
+++ routes/sale.js
@@ -1,41 +1,39 @@
 // Route file for sales operations
 var express = require("express");
 var router = express.Router();
-var db = require("../config/db");
+var client = require("../config/db");
 const { check, validationResult } = require("express-validator");
 var messages = require("../data/messages");
 
 // Get the user's sales
-router.get("/", (req, res) => {
-  // Gets the userId from the authentication middleware
-  var userId = req.userId;
+router.get("/", async (req, res) => {
+  // Gets the playerId from the authentication middleware
+  var playerId = req.playerId;
   // Gets the card collection
-  let sql = "SELECT * FROM collection WHERE userId = " + userId;
-  let query = db.query(sql, (err, collections) => {
-    if (err) {
-      throw err;
-    }
-    // If there are no results, return error
-    if (!collections.length) {
-      return res.status(404).json({ message: messages.COLLECTION_PROBLEM });
-    }
+  let sql = "SELECT * FROM collection WHERE playerid = " + playerId;
+  let collections = await client.query(sql);
+  if (collections.err) {
+    throw collections.err;
+  }
+  // If there are no results, return error
+  if (!collections.rows.length) {
+    return res.status(404).json({ message: messages.COLLECTION_PROBLEM });
+  }
 
-    // If there is a collection, retrieve the cards.
-    var collectionId = collections[0].id;
-    sql =
-      'SELECT s.price, s.percent, s.quantity, s.date, s.conditionId, s.languageId, s.foil, cg.*, o.name AS "condition", l.name AS "language" FROM sale s LEFT JOIN cardGeneral cg ON s.scryfallId = cg.scryfallId LEFT JOIN cardCondition o ON s.conditionId = o.id LEFT JOIN cardLanguage l ON s.languageId = l.id WHERE collectionId = ' +
-      collectionId +
-      " ORDER BY s.date DESC, cg.name";
-    query = db.query(sql, (err, sales) => {
-      if (err) {
-        throw err;
-      }
-      collections[0].sales = sales;
-      delete collections[0].id;
-      delete collections[0].userId;
-      res.status(200).json(collections[0]);
-    });
-  });
+  // If there is a collection, retrieve the cards.
+  var collectionId = collections.rows[0].id;
+  sql =
+    "SELECT s.price, s.percent, s.quantity, s.date, s.conditionid, s.languageid, s.foil, cg.*, o.name AS condition, l.name AS language FROM sale s LEFT JOIN cardgeneral cg ON s.scryfallid = cg.scryfallid LEFT JOIN cardcondition o ON s.conditionid = o.id LEFT JOIN cardlanguage l ON s.languageid = l.id WHERE collectionid = " +
+    collectionId +
+    " ORDER BY s.date DESC, cg.name";
+  let sales = await client.query(sql);
+  if (sales.err) {
+    throw sales.err;
+  }
+  collections.rows[0].sales = sales.rows;
+  delete collections.rows[0].id;
+  delete collections.rows[0].playerId;
+  res.status(200).json(collections.rows[0]);
 });
 
 module.exports = router;
