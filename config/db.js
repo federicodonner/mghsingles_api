780e2d5d33eb67909ff0bc0297befe2ebb732059
federicodonner <federico.donner@gmail.com>
2022-02-14 23:28:35 -0300

    fix(db): fixed db_password variable name

--- config/db.js
+++ config/db.js
@@ -5,14 +5,14 @@ function connectDatabase() {
     process.env.DB_USER,
     process.env.DB_HOST,
     process.env.DB_DATABASE,
-    process.env.DBPASSWORD,
+    process.env.DB_PASSWORD,
     process.env.DB_PORT
   );
   const client = new Client({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
-    password: process.env.DBPASSWORD,
+    password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
   });
   client.connect((err) => {
